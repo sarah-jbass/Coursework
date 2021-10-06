c71d42e72afbe33f86a961f085c36b634b0951fe
Sarah Bass <sbass3@wisc.edu>
2021-10-06 15:03:27 -0500

    Computational progress

--- Y2/Fall 2021/Econ 899/PS3/PS3_compute.jl
+++ Y2/Fall 2021/Econ 899/PS3/PS3_compute.jl
@@ -1,29 +1,79 @@
-using Parameters, Plots #import the packages we want
+using Parameters, Plots, CSV, Tables, DataFrames
 include("PS3_functions.jl") #import the functions that solve our growth model
 
 prim, res = Initialize() #initialize primitive and results structs
-@elapsed Solve_model(prim, res) #solve the model! @elapsed will time the functions, @time name_of_file.jl also works
-@unpack val_func_e, val_func_u, pol_func_e, pol_func_u, μ = res  #separates the value and policy functions out of the results
+Solve_HH_Problem(prim, res)
+Solve_μ(prim, res)
+# @elapsed Solve_model(prim, res) #solve the model! @elapsed will time the functions, @time name_of_file.jl also works
+@unpack value_function, policy_function, labor_supply, μ = res  #separates the value and policy functions out of the results
 @unpack a_grid = prim
-
 
 ##############Make plots
 #value function
-Plots.plot(a_grid, val_func_e, xlabel="K", ylabel="Value", label="Employed")
-Plots.plot!(a_grid, val_func_u, label="Unemployed", title="Value Function")
-Plots.savefig("02_Value_Function.png")
+Plots.plot(a_grid, value_function[50,:,1], xlabel="Assets", ylabel="Value", title="Value Function")
+Plots.savefig("Value_Function.png")
 
 #policy functions
-Plots.plot(a_grid, pol_func_e, xlabel="K", ylabel="K'", label="Employed")
-Plots.plot!(a_grid, pol_func_u, label="Unemployed", title="Policy Function")
-Plots.savefig("02_Policy_Function.png")
+Plots.plot(a_grid, policy_function[20,:,:], xlabel="Assets", ylabel="New Assets", title="Policy Function")
+Plots.savefig("Policy_Function.png")
 
-#changes in policy function
-pol_func_δe = copy(pol_func_e).-a_grid
-pol_func_δu = copy(pol_func_u).-a_grid
-Plots.plot(a_grid, pol_func_δe, xlabel="K", ylabel="K'-K", label="Employed")
-Plots.plot!(a_grid, pol_func_δu, label="Unemployed", title="Policy Function Changes")
-Plots.savefig("02_Policy_Functions_Changes.png")
+#savings decision
+policy_function_δ = copy(policy_function[20,:,:]).-a_grid
+Plots.plot(a_grid, policy_function_δ[20,:,:], xlabel="Assets", ylabel="Savings", title="Savings Decision")
+Plots.savefig("Labor_Supply.png")
 
-println("All done!")
+#labor supply
+Plots.plot(a_grid, labor_supply[20,:,:], xlabel="Assets", ylabel="Labor Supply", title="Labor Supply")
+Plots.savefig("Labor_Supply.png")
+
 ################################
+
+# Benchmark
+@elapsed bm_ss = Solve_model()  # converges in ~9 iterations
+@elapsed bm_no_ss = Solve_model(θ = 0.0)  # converges in ~11 iterations
+
+# No productivity risk
+@elapsed riskless_ss = Solve_model(z = [0.5, 0.5]) # converges in ~12 iterations
+@elapsed riskless_no_ss = Solve_model(θ = 0.0, z = [0.5, 0.5], λ = 0.1)  # converges in ~52 iterations
+
+# Inelastic labor supply
+@elapsed inelastic_l_ss = Solve_model(γ = 1.0, λ = 0.8) # converges in ~6 iterations
+@elapsed inelastic_l_no_ss = Solve_model(θ = 0.0, γ = 1.0, λ = 0.8) # converges in ~7 iterations
+
+# table 1
+table_1 = create_table([bm_ss, bm_no_ss,
+                        riskless_ss, riskless_no_ss,
+                        inelastic_l_ss, inelastic_l_no_ss])
+
+CSV.write("table_1.csv", table_1)
+
+# Figure 4
+bm_ss.value_function[isinf.(bm_ss.value_function)] .= -1/eps();
+bm_no_ss.value_function[isinf.(bm_no_ss.value_function)] .= -1/eps();
+
+bm_ss_line = reshape(sum(bm_ss.value_function .* bm_ss.μ; dims = [2, 3]), 66, 1)
+bm_no_ss_line = reshape(sum(bm_no_ss.value_function .* bm_no_ss.μ; dims = [2, 3]), 66, 1)
+
+plot([bm_ss_line bm_no_ss_line],
+     legend=:bottomright,
+     labels = ["SS" "No SS"],
+     title="Figure 6")
+
+savefig("value_bm_ss_no_ss.png")
+
+sum(bm_ss_line .< bm_no_ss_line)
+
+
+# Figure 5
+bm_ss_line = reshape(sum(bm_ss.value_function .* bm_ss.μ; dims = [1, 3]), 5000, 1)
+
+bm_no_ss_line = reshape(sum(bm_no_ss.value_function .* bm_no_ss.μ; dims = [1, 3]), 5000, 1)
+
+plot([bm_ss_line[2:5000] bm_no_ss_line[2:5000]],
+     legend=:bottomright,
+     labels = ["SS" "No SS"],
+     title="Figure 4")
+
+savefig("value_bm_ss_no_ss.png")
+
+sum(bm_ss_line .< bm_no_ss_line)
--- Y2/Fall 2021/Econ 899/PS3/PS3_functions.jl
+++ Y2/Fall 2021/Econ 899/PS3/PS3_functions.jl
@@ -13,7 +13,7 @@ using Parameters, CSV, Tables, DataFrames
     β::Float64 = 0.97
 
     #Productivity params
-    η::Array{Float64, 1} = map(x->parse(Float64,x), readlines("ef.txt"))
+    η::Array{Float64, 1} = map(x->parse(Float64,x), readlines(open("ef.txt")))
     nz::Int64 = 2
     Π::Array{Float64,2} = [0.9261 0.0739; 0.0189 0.9811]
     Π₀::Array{Float64,2} = [0.2037 0.7963]
@@ -36,7 +36,7 @@ mutable struct Results #preallocating the results, Results is the type of struct
     e::Array{Float64, 2}
     value_function::Array{Float64}
     policy_function::Array{Float64}
-    policy_function_i::Array{Float64}
+    policy_function_i::Array{Int64}
     labor_supply::Array{Float64}
     μ::Array{Float64}
     k::Float64
@@ -72,7 +72,9 @@ function Initialize()
     prim, res #return deliverables
 end
 
-function Retired_Utility(a::Float64, b::Float64, ap::Float64, r::Float64, γ::Float64, σ::Float64)
+function Retired_Utility(a::Float64, ap::Float64, prim::Primitives, res::Results)
+    @unpack r, b = res #unpack value functions
+    @unpack γ, σ = prim
     c = (1+r)a + b - ap
     if (c>0)
         (c^((1-σ) * γ))/(1 - σ)
@@ -84,23 +86,26 @@ end
 function Retired_Bellman(prim::Primitives, res::Results)
     @unpack r, b = res #unpack value functions
     @unpack γ, σ, β, N, Jᴿ, nz, a_min, a_max, na, a_grid  = prim
-    res.value_function[N,:,1] = Retired_Utility.(a_grid, b, 0, r, γ, σ)
+
+    for ia=1:na
+        res.value_function[N,ia,:] .= Retired_Utility(a_grid[ia], 0.0, prim, res)
+    end
 
     for j=(N-1):-1:Jᴿ
 
-        for a = 1:na
-            a = a_grid[a] #value of a
+        for ia = 1:na
+            a = a_grid[ia] #value of a
             candidate_max = -Inf #bad candidate max
 
-            for ap in 1:na#choice_lower:na #loop over possible selections of k', exploiting monotonicity of policy function
-                ap = a_grid[ap]
-                u = Retired_Utility(a, b, ap, r, γ, σ) #consumption given a' selection
-                val = u + β*res.value_function[j+1,ap,1] #compute value
+            for iap in 1:na#choice_lower:na #loop over possible selections of k', exploiting monotonicity of policy function
+                ap = a_grid[iap]
+                u = Retired_Utility(a, ap, prim, res) #consumption given a' selection
+                val = u + β*res.value_function[j+1,iap,1] #compute value
                 if val>candidate_max #check for new max value
                     candidate_max = val #update max value
-                    res.value_function[j,a,:] = val
-                    res.policy_function[j,a,:] = a_grid[ap] #update policy function
-                    res.policy_function_i[j,a,:] = ap #a_grid index that corresponds
+                    res.value_function[j,ia,:] .= val
+                    res.policy_function[j,ia,:] .= a_grid[iap] #update policy function
+                    res.policy_function_i[j,ia,:] .= iap #a_grid index that corresponds
                 end
             end
         end
@@ -109,13 +114,14 @@ function Retired_Bellman(prim::Primitives, res::Results)
 end
 
 function Labor_Supply(γ::Float64, θ::Float64, e::Float64, w::Float64, r::Float64, a::Float64, ap::Float64)
-    l = (γ*(1-θ)*e*w - (1-γ)*((1+r)*a - a_p)) / ((1-θ)*w*e)
-    if l<0 || l>1
-        println("INVALID LABOR SUPPLY")
-    end
+    interior_solution = (γ*(1-θ)*e*w - (1-γ)*((1+r)*a - ap)) / ((1-θ)*w*e)
+    min(1, max(0, interior_solution))
 end
 
-function Working_Utility(l::Float64, a::Float64, b::Float64, ap::Float64, e::Float64, r::Float64, w::Float64, θ::Float64, γ::Float64, σ::Float64)
+function Working_Utility(l::Float64, a::Float64, ap::Float64, e::Float64, prim::Primitives, res::Results)
+    @unpack θ, w, r, b = res #unpack value functions
+    @unpack γ, σ  = prim
+
     c = w*(1-θ)*e*l + (1+r)*a - ap
     if (c>0)
         (((c^γ) * ((1-l)^(1-γ))) ^ (1-σ)) / (1 - σ)
@@ -126,27 +132,30 @@ end
 
 function Working_Bellman(prim::Primitives, res::Results)
     @unpack θ, e, w, r, b = res #unpack value functions
-    @unpack γ, σ, β, N, Jᴿ, nz, a_min, a_max, na, a_grid  = prim
+    @unpack Π, γ, σ, β, N, Jᴿ, nz, a_min, a_max, na, a_grid  = prim
 
     for j=(Jᴿ-1):-1:1
 
         for z = 1:nz
 
-            for a = 1:na
-                a = a_grid[a] #value of a
+            for ia = 1:na
+                a = a_grid[ia] #value of a
                 candidate_max = -Inf #bad candidate max
 
-                for ap in 1:na#choice_lower:na #loop over possible selections of k', exploiting monotonicity of policy function
-                    ap = a_grid[ap]
+                for iap in 1:na#choice_lower:na #loop over possible selections of k', exploiting monotonicity of policy function
+                    ap = a_grid[iap]
                     l = Labor_Supply(γ, θ, e[j,z], w, r, a, ap)
-                    u = Working_Utility(l, a, b, ap, e[j,z], r, w, θ, γ, σ)
-                    val = u + β*(Π[z,1]*res.value_function[j+1,ap,1] + Π[z,2]*res.value_function[j+1,ap,2])#compute value
+                    if l<0 || l>1
+                        println("INVALID LABOR SUPPLY")
+                    end
+                    u = Working_Utility(l, a, ap, e[j,z], prim, res)
+                    val = u + β*(Π[z,1]*res.value_function[j+1,iap,1] + Π[z,2]*res.value_function[j+1,iap,2])#compute value
                     if val>candidate_max #check for new max value
                         candidate_max = val #update max value
-                        res.value_function[j,a,z] = val
-                        res.policy_function[j,a,z] = a_grid[ap] #update policy function
-                        res.policy_function_i[j,a,z] = ap #a_grid index that corresponds
-
+                        res.value_function[j,ia,z] = val
+                        res.policy_function[j,ia,z] = a_grid[iap] #update policy function
+                        res.policy_function_i[j,ia,z] = iap #a_grid index that corresponds
+                        res.labor_supply[j,ia,z] = l
                     end
                 end
             end
@@ -154,13 +163,13 @@ function Working_Bellman(prim::Primitives, res::Results)
     end
 end
 
-function Solve_HH_problem(prim::Primitives, res::Results)
+function Solve_HH_Problem(prim::Primitives, res::Results)
     Retired_Bellman(prim, res)
     Working_Bellman(prim, res)
 end
 
 function Solve_μ(prim::Primitives, res::Results)
-    @unpack N, n, na, nz = prim
+    @unpack N, n, na, nz, Π, Π₀ = prim
     tol = 1e-5
     init_μ = res.μ
     converged = 0
@@ -169,18 +178,13 @@ function Solve_μ(prim::Primitives, res::Results)
         res.μ = zeros(N, na, nz) #set PMF=0
         res.μ[1, 1, :] = Π₀
         for j = 1:(N-1)
-            for a = 1:na
+            for ia = 1:na
                 for z = 1:nz
-                ap = res.policy_function_i[j,a,z]
-                for zp = 1:nz
-                    res.μ[j+1,ap,zp] = res.μ[j+1,ap,zp] + prim.Π[z,zp]*init_μ[j,a,z]
+                iap = res.policy_function_i[j,ia,z]
+                    for zp = 1:nz
+                        res.μ[j+1,iap,zp] = res.μ[j+1,iap,zp] + Π[z,zp]*init_μ[j,ia,z]
+                    end
                 end
-            end
-            μ_diff = maximum(abs.(res.μ .- init_μ))
-            if μ_diff<tol
-                converged=1
-            else
-                init_μ = res.μ
             end
         end
     end
@@ -193,6 +197,13 @@ function Solve_μ(prim::Primitives, res::Results)
 
     age_weight = reshape(repeat(age_weights/sum(age_weights), na * nz), N, na, nz)
     res.μ = age_weight .* res.μ
+
+    μ_diff = maximum(abs.(res.μ .- init_μ))
+    if μ_diff<tol
+        converged=1
+    else
+        init_μ = res.μ
+    end
 end
 
 function Update_prices(prim::Primitives, res::Results, k::Float64, l::Float64)
@@ -202,142 +213,78 @@ function Update_prices(prim::Primitives, res::Results, k::Float64, l::Float64)
     res.b = (res.θ * res.w * l) / sum(res.μ[Jᴿ:N, :, :])
 end
 
-# #Value function iteration
-# function V_iterate(prim::Primitives, res::Results; tol::Float64 = 1e-4, err_e::Float64 = 100.0, err_u::Float64 = 100.0)
-#     n = 0 #counter
+function Calculate_labor_supply(prim::Primitives, res::Results)
+    @unpack Jᴿ, na, nz = prim
+    e_3d = reshape(repeat(res.e, na), Jᴿ -1, na, nz)
+    sum(res.μ[1:(Jᴿ - 1),:,:] .* res.labor_supply .* e_3d)
+end
+
+function Calculate_capital_supply(prim::Primitives, res::Results)
+    @unpack a_grid, N, nz, na, N = prim
+    a_grid_3d = permutedims(reshape(repeat(a_grid, N * nz), na, N, nz), (2, 1, 3))
+    sum(res.μ .* a_grid_3d)
+end
+
+function Solve_model(prim::Primitives, res::Results, λ::Float64 = 0.25)
+    # @unpack γ = prim
+    # @unpack θ = res
+
+    k_0 = 3.3
+    l_0 = 0.3
+
+    Update_prices(prim, res, k_0, l_0)
+
+    ε = 0.001  # tolerance
+    i = 0      # counter
+    diff = 1
+
+    while diff > ε
+        i += 1
+        println("Iteration #", i)
+        println("Capital demand: ", k_0)
+        println("Labor demand: ", l_0)
+
+        Solve_HH_Problem(prim, res)
+        Solve_μ(prim, res)
+
+        k_1 = Calculate_capital_supply(prim, res)
+        l_1 = Calculate_labor_supply(prim, res)
+
+        println("Capital supply: ", k_1)
+        println("Labor supply: ", l_1)
+
+        diff = abs(k_0 - k_1) + abs(l_0 - l_1)
+
+        println("Absolute difference: ", diff)
+        println("************************************")
+
+        k_0 = λ * k_1 + (1 - λ) * k_0
+        l_0 = λ * l_1 + (1 - λ) * l_0
+        Update_prices(prim, res, k_0, l_0)
+    end
+    res.k = k_0
+    res.l = l_0
+    res
+end
 #
-#     while err_e>tol || err_u>tol#begin iteration
-#         v_next_e, v_next_u = Bellman(prim, res) #spit out new vectors, next guess
+# function Process_results(res::Results)
+#     # calculate total welfare
+#     welfare = res.value_function .* res.μ
+#     welfare = sum(welfare[isfinite.(welfare)])
 #
-#         err_e = abs.(maximum(v_next_e.-res.val_func_e))/abs(v_next_e[prim.na, 1]) #reset error level, initial res.val_func is just 0
-#         err_u = abs.(maximum(v_next_u.-res.val_func_u))/abs(v_next_u[prim.na, 1])
+#     # calculate coefficient of variation of wealth
+#     @unpack a_grid, N, nz, na, N = Primitives()
+#     a_grid_3d = permutedims(reshape(repeat(a_grid, N * nz), na, N, nz), (2, 1, 3))
+#     wealth_mean = sum(res.μ .* a_grid_3d)
+#     wealth_second_moment = sum(res.μ .* a_grid_3d .^ 2)
+#     wealth_second_central_moment = wealth_second_moment - wealth_mean^2
+#     cv = wealth_mean / sqrt(wealth_second_central_moment)
 #
-#         res.val_func_e = v_next_e
-#         res.val_func_u = v_next_u #update value function
-#         n+=1
-#     end
-#     println("Value function converged in ", n, " iterations.")
-#
-#     #T* operator - finding stationary distribution
-#     pmf = prim.a_pmf
-#     #pmf_prime = zeros(1000,2) #is this syntax wrong?
-#     pmf_converged = 0
-#
-#     if pmf_converged == 0
-#         res.pmf_prime = zeros(prim.na,2)
-#          for i_a = 1:prim.na
-#             i_ap_e = res.pol_func_i_e[i_a]
-#             i_ap_u = res.pol_func_i_u[i_a]
-#             res.pmf_prime[i_ap_e,1] = res.pmf_prime[i_ap_e,1] + pmf[i_a,1]*prim.pee
-#             res.pmf_prime[i_ap_e,2] = res.pmf_prime[i_ap_e,2] + pmf[i_a,1]*prim.peu
-#             res.pmf_prime[i_ap_u,1] = res.pmf_prime[i_ap_u,1] + pmf[i_a,2]*prim.pue
-#             res.pmf_prime[i_ap_u,2] = res.pmf_prime[i_ap_u,2] + pmf[i_a,2]*prim.puu
-#         end
-#         if sum(res.pmf_prime) > 1+10^(-9)
-#             println("Warning: pmf sums to ", sum(res.pmf_prime), '.')
-#         end
-#         pmf_diff = maximum(abs.(res.pmf_prime .- pmf))
-#         if pmf_diff<tol
-#             pmf_converged=1
-#         else
-#             pmf = res.pmf_prime
-#         end
-#     end
-#     prim.a_pmf = pmf
+#     # create vector of summary statistics
+#     [res.θ, res.γ, res.z[1], res.k, res.l, res.w, res.r, res.b, welfare, cv]
 # end
 #
-# #solve the model
-# function Solve_model(prim::Primitives, res::Results)
-#     tol::Float64 = 1e-2
-#     tol2::Float64 = 10e-6
-#     q_min::Float64 = prim.β
-#     q_max::Float64 = 1.0
-#     mc::Float64 = 1.0
-#     V_iterate(prim, res) #in this case, all we have to do is the value function iteration!
-#     mc = sum(res.pol_func_e .* res.pmf_prime[:,1] + res.pol_func_u .* res.pmf_prime[:,2])
-#     mc1::Float64 = 0
-#     mc2::Float64 = 0
-#     mc3::Float64 = 0
-#     while abs(mc) > tol && (q_max - q_min)/2 > tol2
-#         #Find 3 q values to test
-#         q1 = (3q_min + q_max)/4
-#         q2 = (q_min + q_max)/2
-#         q3 = (q_min + 3q_max)/4
-#         #Find 3 asset clearing values
-#         for i = 1:3
-#             if i == 1
-#                 prim.q = q1
-#                 V_iterate(prim, res)
-#                 mc1 = sum(res.pol_func_e .* res.pmf_prime[:,1] + res.pol_func_u .* res.pmf_prime[:,2])
-#                 println("MC1 is ", mc1)
-#             elseif i == 2
-#                 prim.q = q2
-#                 V_iterate(prim, res)
-#                 mc2 = sum(res.pol_func_e .* res.pmf_prime[:,1] + res.pol_func_u .* res.pmf_prime[:,2])
-#                 println("MC2 is ", mc2)
-#             else
-#                 prim.q = q3
-#                 V_iterate(prim, res)
-#                 mc3 = sum(res.pol_func_e .* res.pmf_prime[:,1] + res.pol_func_u .* res.pmf_prime[:,2])
-#                 println("MC3 is ", mc3)
-#             end
-#         end
-#         #Replace mc value, q, q_min, q_max with new values
-#         if abs(mc1) - minimum([abs(mc1) abs(mc2) abs(mc3)]) < tol2
-#             mc = mc1
-#             prim.q = q1
-#             q_max = q2
-#         elseif  abs(mc2) - minimum([abs(mc1) abs(mc2) abs(mc3)]) < tol2
-#             mc = mc2
-#             prim.q = q2
-#             q_min = q1
-#             q_max = q3
-#         else abs(mc3) - minimum([abs(mc1) abs(mc2) abs(mc3)]) < tol2
-#             mc = mc3
-#             prim.q = q3
-#             q_min = q2
-#         end
-#         println("Weighted sum of assets is ", mc)
-#     end
+# function Create_table(results_vector::Array{Results})
+#     table = DataFrames.DataFrame(Tables.table(reduce(hcat,process_res.(results_vector))'))
+#     rename!(table, [:theta, :gamma, :z_H, :k, :l, :w, :r, :b, :welfare, :cv])
 # end
-#
-# function calculate_wealth_distribution(results::Results)
-#     @unpack a_grid, ee, eu, na = Primitives()
-#
-#     w = zeros(2, na)
-#
-#     for i_s = 1:2
-#         for i_a = 1:na
-#
-#             i_w = argmin(abs.(a_grid[i_a] .+ S[i_s] .- a_grid))
-#
-#             w[i_w, i_s] = results.μ[i_a, i_s]
-#         end
-#     end
-#     w
-# end
-#
-# function calculate_lorenz_curve(w::Array{Float64, 2})
-#     @unpack a_grid, na = Primitives()
-#
-#     x = cumsum(w[:,1] .+ w[:,2])
-#     y = cumsum((w[:,1] .+ w[:,2]) .* a_grid)
-#
-#     unique([x/x[a_length] y/y[a_length]]; dims = 1)
-# end
-#
-# # https://en.wikipedia.org/wiki/Gini_coefficient
-# function calculate_gini(l::Array{Float64, 2})
-#     widths = diff(l[:,1])
-#     heights = ((l[1:end-1,1] .+ l[2:end,1])./2 .- (l[1:end-1,2] .+ l[2:end,2])./2)
-#     a = sum(widths .* heights)
-#
-#     l_pos = l[l[:,2].>0, :]
-#     widths = diff(l_pos[:,1])
-#     heights = (l_pos[1:end-1,2] .+ l_pos[2:end,2])./2
-#     b = sum(widths .* heights)
-#
-#     a/(a+b)
-# end
-#
-# ##############################################################################
